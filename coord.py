611b571947082bde6a5e29c0ff8af2fbda657fd4
Adriano Andrade <a3gtrk2@hotmail.com>
2019-12-06 15:58:27 +0000

    Update cood.py

--- coord.py
+++ coord.py
@@ -4,11 +4,13 @@ from ev3dev2.motor import MoveTank
 from ev3dev2.motor import MotorSet
 from ev3dev2.motor import MoveDifferential
 
-STUD_MM = 8
+STUD_MM = 9.3
 # test with a robot that:
 # - uses the standard wheels known as EV3Tire
 # - wheels are 16 studs apart
-mdiff = MoveDifferential(OUTPUT_A, OUTPUT_D, EV3Tire, 16 * STUD_MM)
+mdiff = MoveDifferential(OUTPUT_A, OUTPUT_D, EV3Tire, 14 * STUD_MM)
+
+SPEEDRPM = 40
 
 def move():
     '''
@@ -16,28 +18,33 @@ def move():
     tank_drive.on_for_rotations(50, 75, 10)
     # drive in a turn for 10 rotations of the outer motor
     '''
-
-    # Rotate 90 degrees clockwise
-    mdiff.turn_right(SpeedRPM(40), 90)
-
-    # Drive forward 500 mm
-    mdiff.on_for_distance(SpeedRPM(40), 500)
-
-    # Drive in arc to the right along an imaginary circle of radius 150 mm.
-    # Drive for 700 mm around this imaginary circle.
-    mdiff.on_arc_right(SpeedRPM(80), 150, 700)
-
     # Enable odometry
     mdiff.odometry_start()
 
-    # Use odometry to drive to specific coordinates
-    mdiff.on_to_coordinates(SpeedRPM(40), 300, 300)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 250, 0)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 500, 0)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 500, 750)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 250, 750)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 750, 750)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 750, 250)
+
+    # Rotate 90 degrees clockwise
+    mdiff.turn_right(SpeedRPM(SPEEDRPM), 90)
+
+    # Drive forward 500 mm
+    mdiff.on_for_distance(SpeedRPM(SPEEDRPM), 500)
+
+    mdiff.turn_left(SpeedRPM(SPEEDRPM), 90)
+
+    mdiff.on_for_distance(SpeedRPM(SPEEDRPM), 500)
+
+    mdiff.turn_left(SpeedRPM(SPEEDRPM), 90)
+
+    mdiff.on_for_distance(SpeedRPM(SPEEDRPM), 1000)
 
     # Use odometry to go back to where we started
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, 0)
+    mdiff.on_to_coordinates(SpeedRPM(SPEEDRPM), 0, 0)
 
-    # Use odometry to rotate in place to 90 degrees
-    mdiff.turn_to_angle(SpeedRPM(40), 90)
 
     # Disable odometry
     mdiff.odometry_stop()
